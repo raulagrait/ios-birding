c5f6a0be1e1b3acdd0189c0c15efb45956e0df49
Raul Agrait <raul.agrait@rd.io>
2015-05-03 09:11:30 -0700

    CodePath: Birder: Add compose view controller skeleton

---
+++ Birder/Birder/View Controllers/ComposeViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ComposeViewController.swift
+//  Birder
+//
+//  Created by Raul Agrait on 5/3/15.
+//  Copyright (c) 2015 rateva. All rights reserved.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onCancelTouched(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func onTweet(sender: AnyObject) {
+        println("post the tweet")
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
